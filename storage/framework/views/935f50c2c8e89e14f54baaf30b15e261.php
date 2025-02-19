3ddc9d4481c418af690b117c8ff4c7b234271f9e
promise <promisejoseph444@gmail.com>
2025-02-19 17:53:07 +0100

    added new modifications

--- storage/framework/views/935f50c2c8e89e14f54baaf30b15e261.php
+++ storage/framework/views/935f50c2c8e89e14f54baaf30b15e261.php
@@ -2,15 +2,15 @@
 <title>Holopals.login</title>
 <?php $__env->stopSection(); ?>
 <?php $__env->startSection('bodyContent'); ?>
-
-<main class="main  border-rounded shadow-sm bg-dark bg-transparent " >
+<div class="background-wrapper">
+<main class="main shadow-sm " >
   <div class="forms">
   <form method="post" action="<?php echo e(route('login')); ?>" enctype="multipart/form-data">
   <?php echo csrf_field(); ?>
   <p class="fs-2 col-lg-auto me-lg-3">HOLO<span>PALS</span></p>
     <h1 class="h3 mb-3 fw-normal fs-5">Please login </h1>
 
-    <div class="form-floating">
+    <div class="form-floating mt-4 text-black w-50 mx-auto">
       <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" value="<?php echo e(old('email')); ?>" >
       <label for="floatingInput">Email address</label>
     </div>
@@ -28,7 +28,7 @@ if (isset($__messageOriginal)) { $message = $__messageOriginal; }
 endif;
 unset($__errorArgs, $__bag); ?>
 
-    <div class="form-floating">
+    <div class="form-floating mt-4 text-black w-50 mx-auto">
       <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password">
       <label for="floatingPassword">Password</label>
     </div>
@@ -67,12 +67,10 @@ unset($__errorArgs, $__bag); ?>
        
     </ul>
 
-    <button class="w-100 btn btn-lg btn-danger mt-4" type="submit" name="submit">	
+    <button class="btn btn-lg btn-danger mt-4 w-25 mx-auto" type="submit" name="submit">	
     	Login
     </button>
 
-    
-  
         <div class=" offset-md-1 mb-2 mt-2">
                 <div class="form-check">
                     <input class="form-check-input " type="checkbox" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
@@ -99,6 +97,7 @@ unset($__errorArgs, $__bag); ?>
   </form>
   </div>
 </main>
+</div>
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layouts.logslayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\laravel\Holoshop\resources\views/auth/login.blade.php ENDPATH**/ ?>
